a8df687b8180d9aa98cb8debf55b0b532d39d2d3
roopashree <roopashree.chennapa@drmartens.com>
2022-06-21 18:34:09 +0100

    Initial commit

---
+++ read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p 'Username: ' uservar
+read -p 'Env: ' envvar
+read -sp 'Enter password: ' passvar
+echo
+echo Thank you $uservar we would be deploying application into $envvar enviroment
